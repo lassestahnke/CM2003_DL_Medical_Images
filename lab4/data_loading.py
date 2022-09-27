f0487a0ffaee507326c070da2ecc106f6acfb88c
lasse <lasse@comr.de>
2022-09-27 18:15:45 +0200

    Modified Dataloader: Added train_generator function to generate and combine image and mask generators

--- lab4/data_loading.py
+++ lab4/data_loading.py
@@ -4,14 +4,76 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 import os
 
-def load_data(base_path, img_path, target_path):
+
+# following function is adopted from https://www.kaggle.com/code/lqdisme/brain-mri-segmentation-unet-keras
+def train_generator(data_frame, batch_size, aug_dict,
+                    image_color_mode="grayscale",
+                    mask_color_mode="grayscale",
+                    target_size=(256, 256),
+                    seed=1337):
+    image_datagen = ImageDataGenerator(**aug_dict)
+    mask_datagen = ImageDataGenerator(**aug_dict)
+
+    image_generator = image_datagen.flow_from_dataframe(
+        data_frame,
+        x_col="img_list",
+        class_mode=None,
+        color_mode=image_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        seed=seed)
+
+    mask_generator = mask_datagen.flow_from_dataframe(
+        data_frame,
+        x_col="mask_list",
+        class_mode=None,
+        color_mode=mask_color_mode,
+        target_size=target_size,
+        batch_size=batch_size,
+        seed=seed)
+
+    train_gen = zip(image_generator, mask_generator)
+
+    for (img, mask) in train_gen:
+        img, mask = adjust_data(img, mask)
+        yield (img, mask)
+
+
+def adjust_data(img, mask):
+    img = img / 255.
+    mask = mask / 255.
+    mask[mask > 0.5] = 1
+    mask[mask <= 0.5] = 0
+
+    return (img, mask)
+
+
+
+def load_data(base_path, img_path, target_path, img_size, batch_size, augmenation_dic):
+    """
+        Function to load data and return a ImageDataGenerator Object for model training
+
+        args:
+            base_path: [string] Path to main directory.
+            img_path: [string] Name of image directory.
+            target_path: [string] Name of mask directory.
+            img_size: [tuple] size of image. (img_width, img_height, img_channel).
+            batch_size: [int] Number of samples per batch.
+            augmentation_dic: [dict] Dictionary of augmentation arguments.
+                                    See tf.keras.preprocessing.image.ImageDataGenerator doc for info.
+        returns:
+            data_gen: [iterator]: Training generator
+    """
     img_list = os.listdir(os.path.join(base_path,img_path))
     target_list = os.listdir(os.path.join(base_path,target_path))
-    data = pd.DataFrame(data={'x': img_list, 'y': target_list})
+    data = pd.DataFrame(data={'img_list': img_list, 'mask_list': target_list})
 
-    datagen = ImageDataGenerator(rescale=1./255)
+    data_gen = train_generator()
+
+
+
     # use flow_from_dataframe
-    return data
+    return data_gen
 
 base_path = "/DL_course_data/Lab3/X_ray"
 masks = "Mask"
