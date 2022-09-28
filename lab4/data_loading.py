1ff8b3a9ac1f0830dcb2a2e4961244380e0edf2a
lasse <lasse@comr.de>
2022-09-28 17:24:36 +0200

    PEP8 style changes

--- lab4/data_loading.py
+++ lab4/data_loading.py
@@ -14,6 +14,7 @@ def train_generator(data_frame, batch_size, aug_dict,
     image_datagen = ImageDataGenerator(**aug_dict)
     mask_datagen = ImageDataGenerator(**aug_dict)
 
+    # creating instance of ImageDataGenerator for loading and augmenting images
     image_generator = image_datagen.flow_from_dataframe(
         data_frame,
         x_col="img_list",
@@ -23,6 +24,7 @@ def train_generator(data_frame, batch_size, aug_dict,
         batch_size=batch_size,
         seed=seed)
 
+    # creating instance of ImageDataGenerator for loading and augmenting masks
     mask_generator = mask_datagen.flow_from_dataframe(
         data_frame,
         x_col="mask_list",
@@ -32,21 +34,21 @@ def train_generator(data_frame, batch_size, aug_dict,
         batch_size=batch_size,
         seed=seed)
 
+    # combining both generators into one
     train_gen = zip(image_generator, mask_generator)
 
+    # rescale image and masks and return generator
     for (img, mask) in train_gen:
         img, mask = adjust_data(img, mask)
         yield (img, mask)
 
-
+    # scaling of grayscale image and discretizing mask values
 def adjust_data(img, mask):
     img = img / 255.
     mask = mask / 255.
     mask[mask > 0.5] = 1
     mask[mask <= 0.5] = 0
-
     return (img, mask)
-
 
 
 def load_data(base_path, img_path, target_path, img_size, batch_size, augmenation_dic):
@@ -64,21 +66,18 @@ def load_data(base_path, img_path, target_path, img_size, batch_size, augmenatio
         returns:
             data_gen: [iterator]: Training generator
     """
-    img_list = os.listdir(os.path.join(base_path,img_path))
-    target_list = os.listdir(os.path.join(base_path,target_path))
+
+    # get image and mask paths and prepare pd.DataFrame
+    img_list = os.listdir(os.path.join(base_path, img_path))
+    target_list = os.listdir(os.path.join(base_path, target_path))
     data = pd.DataFrame(data={'img_list': img_list, 'mask_list': target_list})
 
+    # get training generator for images and masks
     data_gen = train_generator()
-
-
-
-    # use flow_from_dataframe
     return data_gen
+
 
 base_path = "/DL_course_data/Lab3/X_ray"
 masks = "Mask"
 img = "Image"
 print(load_data(base_path, img, masks))
-
-
-
